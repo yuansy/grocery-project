8bd6353eada9f22bb5338826e3e3588ccfcfaf61
yuansy <sinyi5997@gmail.com>
2020-03-19 19:17:31 +0800

    actions

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,14 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
+    apiKey: "AIzaSyCmvsPKy4RyEMpu0GLdnFVd-KmZSOS7E9Q",
+    authDomain: "grocery-project-a35dc.firebaseapp.com",
+    databaseURL: "https://grocery-project-a35dc.firebaseio.com",
+    projectId: "grocery-project-a35dc",
+    storageBucket: "grocery-project-a35dc.appspot.com",
+    messagingSenderId: "678228557804",
+    appId: "1:678228557804:web:4f81406957e8a215d9cac9",
+    measurementId: "G-0K4SRSG539"
   };
   
   firebase.initializeApp(firebaseConfig);

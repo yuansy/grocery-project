19a2a5621335d71855b581047d699dd146fd216f
Thangamanir <thangamani.r@gmail.com>
2020-03-19 17:18:05 +0800

    Updated firebase.js

--- grocerychart/src/firebase.js
+++ grocerychart/src/firebase.js
@@ -2,10 +2,10 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-    apiKey: "AIzaSyDR5ZGNKSbpZ71uXOtH99-tX5qSWzgBr9M",
-    authDomain: "bt3103-2020.firebaseapp.com",
-    databaseURL: "https://bt3103-2020.firebaseio.com",
-    projectId: "bt3103-2020"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: ""
 
   };
   
